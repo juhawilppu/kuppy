8f4be60f562d8bf7dc21ab15314eb8a895f5d21b
Juha Rajajärvi <rajajarvi@gmail.com>
2016-02-20 11:49:32 +0200

    CupJS now supports clicking players' to proceed them to next round

--- README.md
+++ README.md
@@ -1,36 +1,48 @@
 CupJS
 =====
 
-JavaScript library to build a cup bracket. Takes the number of participants (4, 8, 16, 32, etc.) as a parameter. Create a div with id "tournament".
+JavaScript library to build a cup bracket.
 
-See example: https://rawgithub.com/juharajajarvi/cupjs/master/example.html
+**Features**
+*Supports any number of participants (4, 8, 16, 32, etc).
+*Proceed players to following rounds by clicking the name of the player. Also possible by calling the JavaScript object.
+
+See it in action: https://rawgithub.com/juharajajarvi/cupjs/master/example.html
 
 Usage:
 ```javascript
         var testCup = new Cup(8);
         testCup.buildBracket();
         
+		// 1st round, 1st pair
         testCup.addPlayer(1,1,'UPPER', 'Jake');
         testCup.addPlayer(1,1,'BOTTOM', 'Patrick');
 
+		// 1st round, 2nd pair
 		testCup.addPlayer(1,2,'UPPER', 'Michael');
         testCup.addPlayer(1,2,'BOTTOM', 'Nigel');
 		
+		// 1st round, 3rd pair
 		testCup.addPlayer(1,3,'UPPER', 'Robin');
         testCup.addPlayer(1,3,'BOTTOM', 'Peter');
 
+		// 1st round, 4th pair
 		testCup.addPlayer(1,4,'UPPER', 'Aaron');
         testCup.addPlayer(1,4,'BOTTOM', 'Theo');
 		
+		// 2nd round, 1st pair
         testCup.addPlayer(2,1,'UPPER', 'Jake');		
 		testCup.addPlayer(2,1,'BOTTOM', 'Michael');
 
+		// 2nd round, 2nd pair
         testCup.addPlayer(2,2,'UPPER', 'Robin');		
 		testCup.addPlayer(2,2,'BOTTOM', 'Theo');		
 
+		// 3rd round, 1st pair
         testCup.addPlayer(3,1,'UPPER', 'Michael');		
 		testCup.addPlayer(3,1,'BOTTOM', 'Theo');
 
+		// Winner
         testCup.setWinner('Theo');
 		
 ```
--- cup.js
+++ cup.js
@@ -1,197 +1,244 @@
 /*
  * CupJS -- library for building a cup bracket for a tournament.
- * 
+ *
  * Copyright 2016 Juha Rajajärvi
  * Released under the MIT license
  * https://jquery.org/license
  *
  */
 
-
 function Cup(elementId, numberOfPlayers) {
 
-    this.$tournament = $(elementId);	
-	this.$tournament.addClass('cupjs');	
-    this.numberOfPlayers = numberOfPlayers;
+	this.$tournament = $(elementId);
+	this.$tournament.addClass('cupjs');
+	this.numberOfPlayers = numberOfPlayers;
 
 	/* Distance between two players that form a pair. */
 	this.DISTANCE_BETWEEN_PLAYERS_IN_A_PAIR = 1.6;
-	
+
 	/* Distance between two pairs. */
-    this.DISTANCE_BETWEEN_PAIRS = 4;
+	this.DISTANCE_BETWEEN_PAIRS = 4;
+
+	/* Distance between rounds in terms of box width. */
+	this.DISTANCE_BETWEEN_ROUNDS = 0.5;
 	
 	/* Words for pair */
 	this.UPPER = 'UPPER';
-	this.BOTTOM = 'BOTTOM';	
-	
+	this.BOTTOM = 'BOTTOM';
+
 	/** Public methods **/
-	
-    this.buildBracket = function() {
 
-        var round = 0;
-        var playersLeft = 0;
-        
-        do {
-            round++;
+	this.buildBracket = function () {
 
-            playersLeft = this.numberOfPlayers / Math.pow(2, round-1);                
-            
-            for (var pair=1; pair <= playersLeft/2; pair++) {
-                this._drawPair(round, pair);
-            }
-        
-        } while ( playersLeft !== 1);
-        
-        this._drawWinnerOfCup(round);
-        
-    };
-    
-    this.addPlayer = function(round, pair, part, name) {
-        this._getElementByMatchId(round+'_'+pair+'_'+part).find('.name').html(name);
-    };
-    
-	this.setWinner = function(name) {
-        this._getElementByMatchId('winner').find('.name').html(name);
-    };
-	
-	
+		var round = 0;
+		var playersLeft = 0;
+
+		do {
+			round++;
+
+			playersLeft = this.numberOfPlayers / Math.pow(2, round - 1);
+
+			for (var pair = 1; pair <= playersLeft / 2; pair++) {
+				this._drawPair(round, pair);
+			}
+
+		} while (playersLeft !== 1);
+
+		this._drawWinnerOfCup(round);
+
+	};
+
+	this.addPlayer = function (round, pair, part, name) {
+		this._getElementByMatchId(round + '_' + pair + '_' + part)
+		.attr('name', name)
+		.find('.name').html(name);
+	};
+
+	this.setWinner = function (name) {
+		this._getElementByMatchId('winner').find('.name').html(name);
+	};
+
 	/** Private methods **/
+
+	this._getElementByMatchId = function (matchId) {
+		return $('div[id="' + matchId + '"]');
+	};
+
+	this._getCoordinatesByMatchId = function (matchId) {
+		var $element = this._getElementByMatchId(matchId);
+		return this._getCoordinates($element);
+	};
+
+	this._getCoordinates = function ($element) {
+
+		var x = $element.offset().left + $element.width();
+		var y = $element.offset().top + $element.height() / 2.0;
+
+		return {
+			x : x,
+			y : y
+		};
+	};
+
+	this._getCoordinatesCss = function ($element) {
+
+		var x = $element.offset().left + $element.width();
+		var y = parseInt($element.css('top'))
+
+			return {
+			x : x,
+			y : y
+		}
+	};
+
+	this._addConnector = function (matchId1, matchId2, longVersion) {
+
+		var offset = 0;
+
+		if (longVersion === true) {
+			offset = this._getElementByMatchId(matchId1).width();
+		}
+
+		var coords1 = this._getCoordinatesByMatchId(matchId1),
+		coords2 = this._getCoordinatesByMatchId(matchId2),
+		$connector = $('<div class="connector"></div>');
+
+		$connector
+		.css('height', (coords2.y - coords1.y) + 'px')
+		.css('width', (100 + offset) + 'px')
+		.css('top', coords1.y)
+		.css('left', (coords1.x - offset))
+
+		$connector.appendTo('body');
+	};
+
+	this._drawPair = function (round, pair) {
+
+		// First round does not have connectors
+		if (round === 1) {
+			this._drawPairForFirstRound(round, pair);
+		} else {
+			this._drawPairForRound(round, pair);
+		}
+
+	};
+
+	this._drawWinnerOfCup = function (round) {
+		var id = 'winner';
+		var parents = this._getParentsId(round, 1, true);
+
+		this._drawWinner(round, 1, this.UPPER, parents);
+	};
+
+	this._drawWinner = function (round, pair, upperBottom, parents) {
+
+		var $element = this._createPlayerBoxElement(round, pair, upperBottom);
+
+		$element.appendTo(this.$tournament);
+		
+		this._addClickListenerForProceedingToNextLevel($element);
+
+		var roundOffset = $element.width() * this.DISTANCE_BETWEEN_ROUNDS;
+
+		var first_coords = this._getCoordinatesCss($("div[id='" + parents.a + "']")),
+		second_coords = this._getCoordinatesCss($("div[id='" + parents.b + "']"));
+
+		$element
+		.css('left', (first_coords.x + second_coords.x) * this.DISTANCE_BETWEEN_ROUNDS + roundOffset + 'px')
+		.css('top', (first_coords.y + second_coords.y) * this.DISTANCE_BETWEEN_ROUNDS + 'px');
+
+	};
+
+	this._getParentsId = function (ownRound, ownPair, isFirst) {
+
+		var parentRound = ownRound - 1;
+		var parentPair = ownPair * 2;
+
+		if (isFirst) {
+			parentPair--;
+		}
+
+		var first_parent = parentRound + '_' + parentPair + '_' + this.UPPER;
+		var second_parent = parentRound + '_' + parentPair + '_' + this.BOTTOM;
+
+		return {
+			a : first_parent,
+			b : second_parent
+		};
+	};
+
+	this._getId = function(round, pair, upperBottom) {
+		return round + '_' + pair + '_' + upperBottom;
+	}
 	
-    this._getElementByMatchId = function(matchId) {
-        return $('div[id="'+matchId+'"]');
-    };
+	this._createPlayerBoxElement = function(round, pair, upperBottom) {
+		var generated_id = this._getId(round, pair, upperBottom);
+		return $('<div id="' + generated_id + '" round="'+round+'" pair="'+pair+'" upperBottom="'+upperBottom+'" class="box"><div class="name"></div></div>');
+	}
+	
+	this._addClickListenerForProceedingToNextLevel = function($element) {
+		var that=this;
+		$element.on('click', function() {
 
-    this._getCoordinatesByMatchId = function(matchId) {
-        var $element = this._getElementByMatchId(matchId);
-        return this._getCoordinates($element);
-    };
+			var round = parseInt($(this).attr('round'));
+			var pair = parseInt($(this).attr('pair'));			
+			var name = $(this).attr('name');	
+			
+			var nextRound = round+1;
+			var nextPair = Math.ceil(pair/2);
+			var nextUpperBottom;
+			if (pair % 2 == 0) {
+				nextUpperBottom = that.BOTTOM;
+			} else {
+				nextUpperBottom = that.UPPER;
+			}
+			
+			that.addPlayer(nextRound, nextPair, nextUpperBottom, name);
+		});
+	}
+	
+	this._drawPairForFirstRound = function (round, pair) {
 
-    this._getCoordinates = function($element) {
+		var $firstPlayer = this._createPlayerBoxElement(round, pair, this.UPPER);
+		var $secondPlayer = this._createPlayerBoxElement(round, pair, this.BOTTOM);		
 
-        var x = $element.offset().left + $element.width();
-        var y = $element.offset().top + $element.height()/2.0;
-        
-        return {x:x,y:y};
-    };
+		$firstPlayer.appendTo(this.$tournament);
+		$secondPlayer.appendTo(this.$tournament);
+		
+		this._addClickListenerForProceedingToNextLevel($firstPlayer);
+		this._addClickListenerForProceedingToNextLevel($secondPlayer);
+		
+		var elementHeight = $firstPlayer.height();
+		var elementWidth = $firstPlayer.width();
 
-    this._getCoordinatesCss = function($element) {
+		var offset_first_y = (pair - 1) * (elementHeight * this.DISTANCE_BETWEEN_PAIRS);
+		var offset_second_y = offset_first_y + elementHeight * this.DISTANCE_BETWEEN_PLAYERS_IN_A_PAIR;
 
-        var x = $element.offset().left + $element.width();
-        var y = parseInt( $element.css('top') )
-        
-        return {x:x,y:y}
-    };
+		$firstPlayer
+		.css('top', offset_first_y + 'px');
 
-    this._addConnector = function(matchId1, matchId2, longVersion) {
+		$secondPlayer
+		.css('top', offset_second_y + 'px');
 
-        var offset = 0;
+		this._addConnector(
+			this._getId(round, pair, this.UPPER),
+			this._getId(round, pair, this.BOTTOM)
+		);
+	};
+	
+	this._drawPairForRound = function (round, pair) {
 
-        if ( longVersion === true ) {
-            offset = this._getElementByMatchId(matchId1).width();
-        }
+		var a_parents = this._getParentsId(round, pair, true);
+		var b_parents = this._getParentsId(round, pair, false);
 
-        var coords1 = this._getCoordinatesByMatchId(matchId1),
-            coords2 = this._getCoordinatesByMatchId(matchId2),
-            $connector = $('<div class="connector"></div>');
+		this._drawWinner(round, pair, this.UPPER, a_parents);
+		this._drawWinner(round, pair, this.BOTTOM, b_parents);
 
-        $connector
-            .css('height', (coords2.y - coords1.y) +'px' )
-            .css('width', (100+offset) +'px' )
-            .css('top', coords1.y)
-            .css('left', (coords1.x-offset))
-            
-        $connector.appendTo('body');
-    };
+		this._addConnector(
+			this._getId(round, pair, this.UPPER),
+			this._getId(round, pair, this.BOTTOM)
+		);
 
-    this._drawPair = function(round, pair) {
-
-        if ( round === 1 ) {
-            this._drawPairForFirstRound(round, pair);
-        } else {
-            this._drawPairForRound(round, pair);
-        }
-
-    };
-    
-    this._drawWinnerOfCup = function(round) {
-        var id = 'winner';
-        var parents = this._getParentsId(round, 1, true);
-        
-        this._drawWinner(id, parents);
-    };
-    
-    this._drawWinner = function(ownId, parents) {
-        
-        var $element = $('<div id="'+ownId+'" class="box"><div class="name"></div></div>');
-    
-        $element.appendTo( this.$tournament );
-        
-        var roundOffset = $element.width()/2.0;
-                
-        var first_coords = this._getCoordinatesCss( $("div[id='"+parents.a+"']") ),
-            second_coords = this._getCoordinatesCss( $("div[id='"+parents.b+"']") );
-                
-        $element
-            .css('left', (first_coords.x+second_coords.x)/2.0 + roundOffset + 'px')
-            .css('top',  (first_coords.y+second_coords.y)/2.0 + 'px');
-            
-    };
-    
-    this._getParentsId = function(ownRound, ownPair, isFirst) {
-    
-        var parentRound = ownRound-1;
-        
-        var parentPair = ownPair*2;
-    
-        if (isFirst) {
-            parentPair--;
-        }
-    
-        var first_parent = parentRound + '_' + parentPair  + '_' + this.UPPER;
-        var second_parent = parentRound + '_' + parentPair  + '_' + this.BOTTOM;
-        
-        return {a:first_parent, b:second_parent};
-    };
-    
-    this._drawPairForRound = function(round, pair) {
-    
-        var a_id = round + '_' + pair + '_' + this.UPPER;
-        var b_id = round + '_' + pair + '_' + this.BOTTOM;
-
-        var a_parents = this._getParentsId(round, pair, true);
-        var b_parents = this._getParentsId(round, pair, false);
-
-        this._drawWinner(a_id, a_parents);
-        this._drawWinner(b_id, b_parents);    
-            
-        this._addConnector(a_id, b_id);
-        
-    };
-    
-    this._drawPairForFirstRound = function(round, pair) {
-    
-        var firstId = round + '_' + pair + '_' + this.UPPER;
-        var secondId = round + '_' + pair + '_' + this.BOTTOM;
-        
-        var $firstPlayer = $('<div id="'+firstId+'" class="box"><div class="name"></div><div class="score"></div></div>');
-        var $secondPlayer = $('<div id="'+secondId+'" class="box"><div class="name"></div><div class="score"></div></div>');
-                
-        $firstPlayer.appendTo( this.$tournament );
-        $secondPlayer.appendTo( this.$tournament );
-            
-        var elementHeight = $firstPlayer.height();
-        var elementWidth = $firstPlayer.width();
-            
-        var offset_first_y = (pair-1)*(elementHeight*this.DISTANCE_BETWEEN_PAIRS);
-        var offset_second_y = offset_first_y+ elementHeight*this.DISTANCE_BETWEEN_PLAYERS_IN_A_PAIR;
-        
-        $firstPlayer
-            .css('top', offset_first_y+'px');
-        
-        $secondPlayer
-            .css('top', offset_second_y+'px');
-        
-        this._addConnector(firstId, secondId);
-    };
+	};
 
 }
