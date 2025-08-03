4f433f438bfae9f48453a1123541e34358e7d02a
Juha Wilppu <juha.wilppu@gmail.com>
2025-08-03 11:49:07 +0300

    Add video mode (delayed rendering) to demo page

--- demo/main.js
+++ demo/main.js
@@ -7,25 +7,40 @@ window.jQuery = $;
 
 const bracket = new kuppy({ elementId: '#tournament', numberOfPlayers: 8 });
 
-bracket.addPlayer(1, 1, 'UPPER', 'Jake');
-bracket.addPlayer(1, 1, 'BOTTOM', 'Patrick');
+const commands = [
+    () => bracket.addPlayer(1, 1, 'UPPER', 'Jake'),
+    () => bracket.addPlayer(1, 1, 'BOTTOM', 'Patrick'),
+    () => bracket.addPlayer(1, 2, 'UPPER', 'Michael'),
+    () => bracket.addPlayer(1, 2, 'BOTTOM', 'Nigel'),
+    () => bracket.addPlayer(1, 3, 'UPPER', 'Robin'),
+    () => bracket.addPlayer(1, 3, 'BOTTOM', 'Peter'),
+    () => bracket.addPlayer(1, 4, 'UPPER', 'Aaron'),
+    () => bracket.addPlayer(1, 4, 'BOTTOM', 'Theo'),
 
-bracket.addPlayer(1, 2, 'UPPER', 'Michael');
-bracket.addPlayer(1, 2, 'BOTTOM', 'Nigel');
+    () => bracket.addPlayer(2, 1, 'UPPER', 'Jake'),
+    () => bracket.addPlayer(2, 1, 'BOTTOM', 'Michael'),
+    () => bracket.addPlayer(2, 2, 'UPPER', 'Robin'),
+    () => bracket.addPlayer(2, 2, 'BOTTOM', 'Theo'),
 
-bracket.addPlayer(1, 3, 'UPPER', 'Robin');
-bracket.addPlayer(1, 3, 'BOTTOM', 'Peter');
+    () => bracket.addPlayer(3, 1, 'UPPER', 'Michael'),
+    () => bracket.addPlayer(3, 1, 'BOTTOM', 'Theo'),
 
-bracket.addPlayer(1, 4, 'UPPER', 'Aaron');
-bracket.addPlayer(1, 4, 'BOTTOM', 'Theo');
+    () => bracket.addPlayer(4, 1, 'UPPER', 'Theo')
+]
 
-bracket.addPlayer(2, 1, 'UPPER', 'Jake');
-bracket.addPlayer(2, 1, 'BOTTOM', 'Michael');
-
-bracket.addPlayer(2, 2, 'UPPER', 'Robin');
-bracket.addPlayer(2, 2, 'BOTTOM', 'Theo');
-
-bracket.addPlayer(3, 1, 'UPPER', 'Michael');
-bracket.addPlayer(3, 1, 'BOTTOM', 'Theo');
-
-bracket.addPlayer(4, 1, 'UPPER', 'Theo');
+const videoMode = true;
+if (videoMode) {
+    // Run commands with delay
+    setTimeout(() => {
+        commands.forEach((command, index) => {
+            setTimeout(() => {
+                command();
+            }, index * 175);
+        });
+    }, 1000);
+} else {
+    // Run commands immediately
+    commands.forEach((command, index) => {
+        command();
+    });
+}
